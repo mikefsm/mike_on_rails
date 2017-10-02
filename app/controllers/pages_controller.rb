9ed8cc4a40a2313f9c66548c9f0d08cf691c5057
mikefsm <sftmfs@gmail.com>
2017-10-02 11:35:16 -0400

    Add about page and update styling in a couple of places.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
 
+  def about
+    @meta_title = meta_title 'About'
+    @meta_description = 'What is Kaizen and how can it be applied to Ruby on Rails and other development platforms as well as incorporated into life in general.'
+  end
+
   def home
     @posts = Post.all.order(created_at: :desc).limit(3)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
+  get '/about' => 'pages#about', as: :about
 
   resources :posts, only: [:new, :create, :edit, :update, :show, :index, :destroy]
 
